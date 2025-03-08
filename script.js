628dad994e21315c4272a160edae6e73f78b3c1c
Gilvan <87739497+GilvanPedro@users.noreply.github.com>
2025-03-08 19:55:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ document.getElementById('filter-form').addEventListener('submit', function(event
     const tagFilter = document.getElementById('tag-filter').value.toLowerCase();
     const timeFilter = document.getElementById('time-filter').value.toLowerCase();
     const platFilter = document.getElementById('plat-filter').value.toLowerCase();
+    const langFilter = document.getElementById('lang-filter').value.toLowerCase();
 
     // Obtendo todos os itens
     const items = document.querySelectorAll('.item');
@@ -30,14 +31,16 @@ document.getElementById('filter-form').addEventListener('submit', function(event
         const tags = item.getAttribute('data-tags').toLowerCase();
         const time = item.getAttribute('data-time').toLowerCase();
         const day = item.getAttribute('data-plat').toLowerCase();
+        const idioma = item.getAttribute('data-lang').toLowerCase();
 
         // Verifica se o item atende aos filtros
         const matchTag = tagFilter === "" || tags.includes(tagFilter);
         const matchTime = timeFilter === "" || time.includes(timeFilter);
         const matchPlat = platFilter === "" || day.includes(platFilter);
+        const matchLang = langFilter === "" || idioma.includes(langFilter);
 
         // Exibir se todos os filtros forem correspondentes
-        if (matchTag && matchTime && matchPlat) {
+        if (matchTag && matchTime && matchPlat && matchLang) {
             item.style.display = 'block';
         } else {
             item.style.display = 'none';
